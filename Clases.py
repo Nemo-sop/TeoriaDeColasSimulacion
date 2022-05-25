0d0c8f1b33517cbfd22d2d1c76a3f47ad10d68b9
NemoBitBot <84641717+NemoBitBot@users.noreply.github.com>
2022-05-25 18:28:13 -0300

    Initial commit

---
+++ Clases.py
@@ -0,0 +1,13 @@
+class Avion():
+    def __init__(self, estado, tiempo_llegada, tiempo_espera_aire, tiempo_espera_tierra):
+        self.estado = estado
+        self.tiempo_llegada = tiempo_llegada
+        self.tiempo_espera_aire = tiempo_espera_aire
+        self.tiempo_espera_tierra = tiempo_espera_tierra
+
+class Pista():
+    def __init__(self, estado, cola_aire, cola_tierra):
+        self.estado = estado
+        self.cola_aire = cola_aire
+        self.cola_tierra = cola_tierra
+        
---
+++ Simulacion.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+from Clases import *
+import random
+import time
+"""
+parametros = [
+    proxima_llegada,
+    [(finalizacion de permanencia de los aviones q estan)],
+    tiempo_total_simulacion,
+    capacidad_maxima_tierra, 
+    media_distribucion_llegada,
+    [a_despegue, b_despegue],
+    [a_aterrizaje, b_aterrizaje],
+    [media_operacion, dvstd_operacion]
+]
+"""
+def exponencialNegativa(media):
+    rnd = random.random()
+    return -media * np.log(1 - rnd)
+
+
+def normal(media, desvEst):
+    rnds = [random.random() for i in range(12)]
+    return (np.sum(rnds)-6) * desvEst + media
+
+def uniforme(a,b):
+    return a + random.random() * (b - a)
+
+def simular(proxima_llegada, permanencia_inicial, tiempo_total_simulacion, capacidad_maxima_tierra, media_distribucion_llegada, uniforme_despegue, uniforme_aterrizaje, normal_operacion):
+    permanencia_inicial = [7, 9, 15, 17, 20]
+    uniforme_despegue = [4,7]
+    uniforme_aterrizaje = [3,5]
+    normal_operacion = [60, 20]
+
+    tabla = pd.DataFrame(
+        {"Evento": [],
+         "Reloj": [],
+         "LA_RND": [],
+         "LA_tiempo_entre_llegada": [],
+         "LA_proxima_llegada": [],
+         "FA_RND": [],
+         "FA_tiempo_aterrizaje": [],
+         "FA_finalizacion_aterrizaje": [],
+         "FA_RND": [],
+         "FA_RND": [],
+         "FA_RND": [],
+         "FA_RND": []
+         })
